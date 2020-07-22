f775dd548af3595a0639f8fe951c22caff81b77a
johnobla <45395888+johnobla@users.noreply.github.com>
2020-07-22 17:32:58 +0100

    Created tests and requests

--- spec/requests/status_spec.rb
+++ spec/requests/status_spec.rb
@@ -4,8 +4,9 @@ RSpec.describe 'Status requests' do
   describe 'GET /status' do
     it 'returns a status message' do
       get('/status')
-      json = JSON.parse(response.body)
-      expect(json['status']).to eql('ok')
+      response_json = JSON.parse(response.body)
+      expect(response_json['status']).to eql('ok')
+      expect(response.status).to eql(200)
     end
   end
 end
